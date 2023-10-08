dc107fda52e9fa8fa62bca156f7fe521a890dc03
chidamabarsk13 <142134688+chidamabarsk13@users.noreply.github.com>
2023-10-08 19:06:15 +0530

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,27 @@ import PortalTextArea from './PortalTextArea.js';
 
 //complete the function 
 const App = () => {
+  const [inputValue, setInputValue]=useState('');
+
+  const handleButtonClick= () => {
+    ReactDOM.createPortal(
+      inputValue,
+      document.getElementById('portal-textfield')
+    );
+  };
 
 
 
   return (
     
     <div id="main">
-      <input id="input" value={} onChange={}></input><br/>
-      <PortalButton buttonclick={} /><br/>
-      <PortalTextArea value={}/>
+      <input type='text' id="input" value={inputValue} onChange={(e)=>setInputValue(e.target.value)}></input><br/>
+      <div id="portal-button">
+      <PortalButton buttonclick={handleButtonClick} /><br/>
+      </div>
+      <div id="portal-textfield">
+      <PortalTextArea />
+      </div>
     </div>
   )
 }
--- src/components/PortalButton.js
+++ src/components/PortalButton.js
@@ -2,14 +2,14 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 
 //complete this function using portals
-const PortalButton=()=>{
+const PortalButton=({onClick})=>{
     return (
         <div>
-            <button id="button" onClick={}>Click</button>
+            <button id="button" onClick={onClick}>Click</button>
         </div>
         
-    )
-}
+    );
+};
 export default PortalButton;
 
 //portal-button id will be used here for portal purpose
